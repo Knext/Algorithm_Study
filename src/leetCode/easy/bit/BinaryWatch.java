436ae813dab930c62265ed848585a1dd00780aa0
Sean.Kim <knext80@gmail.com>
2018-04-04 23:29:20 +0900

    [63] Binary Watch -2
    map으로 계산 결과를 저장.

--- src/leetCode/easy/bit/BinaryWatch.java
+++ src/leetCode/easy/bit/BinaryWatch.java
@@ -2,12 +2,16 @@ package leetCode.easy.bit;
 
 import java.util.*;
 
-import static java.util.stream.Collectors.toList;
-
 //https://leetcode.com/problems/binary-watch/description/
+//map으로 계산 결과를 저장해서 재활용 하는데, 특별한 속도 개선 효과는 못봄.
 public class BinaryWatch {
+    Map<Integer, List<Integer>> hourMap = new HashMap<>();
+    Map<Integer, List<Integer>> minMap = new HashMap<>();
+
     public List<String> readBinaryWatch(int num) {
         if (num == 0) return Arrays.asList("0:00");
+        hourMap.clear();
+        minMap.clear();
 
         List<String> result = new ArrayList<>();
 
@@ -27,21 +31,31 @@ public class BinaryWatch {
         return result;
     }
 
+
     private int[] hourList(int bitNum) {
+        if (hourMap.containsKey(bitNum))
+            return hourMap.get(bitNum).stream().mapToInt(i->i).toArray();
+
         List<Integer> hours = new ArrayList<>();
         for (int j = 0; j < 12; j++) {
             if (bitNum == Integer.bitCount(j))
                 hours.add(j);
         }
+        hourMap.put(bitNum, hours);
         return hours.stream().mapToInt(i->i).toArray();
     }
 
     private int[] minList(int bitNum) {
+        if (minMap.containsKey(bitNum))
+            return minMap.get(bitNum).stream().mapToInt(i->i).toArray();
+
         List<Integer> mins = new ArrayList<>();
         for (int j = 0; j < 60; j++) {
             if (bitNum == Integer.bitCount(j))
                 mins.add(j);
         }
+
+        minMap.put(bitNum, mins);
         return mins.stream().mapToInt(i->i).toArray();
     }
 }
